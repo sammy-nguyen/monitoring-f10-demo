88fb4e6d656859aeb98b769611b6058965cc61b4
sammy-nguyen <Sambu1227@gmail.com>
2021-10-26 12:38:49 -0600

    added 5

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get("/", (req, res) => {
 });
 
 
+app.get('/style', (req, res) =>{
+    let {name} = req.body
+    res.sendFile(path.join(__dirname, '/public.styles.css'))
+})
 
 
 let students = []
@@ -28,11 +32,24 @@ app.post('/api/student', (req,res) => {
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
-    rollbar.log('Student added successfully', {author:'Sam', type: 'manual entry'})
-    res.status(200).send(students)
+    const index = students.findIndex(studentName => studentName === name)
+
+    if(index  === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author:'Sam', type: 'manual entry'})
+        res.status(200).send(students)
+    }else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('Must provide a name.')
+    }else{
+        rollbar.critical('Student already exist')
+        res.status(400).send('that student already exists');
+    }
 
 })
+
+
+
 
 app.use(rollbar.errorHandler())
 
