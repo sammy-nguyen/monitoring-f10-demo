f7a850b3444229f0acb10e2a0f47747c1c67c252
sammy-nguyen <Sambu1227@gmail.com>
2021-10-26 11:47:53 -0600

    added rollbar 2

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const path = require('path')
-const Rollbar = require('rollbar')
-const app = express()
-
-constRollbar = require('rollbar')
+const express = require("express");
+const path = require("path");
+const Rollbar = require("rollbar");
+const app = express();
 
 let rollbar = new Rollbar({
   accessToken: "5b23a0aec9ae427db1a3d6da67dfd333",
   captureUncaught: true,
-  captureUnhandledRejections: true
+  captureUnhandledRejections: true,
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-    rollbar.info('html file served successfully')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+  rollbar.info("html file served successfully");
+});
 
-const port = process.env.PORT || 4545
-app.listen(port, () => console.log(`take us to warp ${port} !`))
+const port = process.env.PORT || 4545;
+app.listen(port, () => console.log(`take us to warp ${port}!`));
