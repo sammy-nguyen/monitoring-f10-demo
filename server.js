880febf07ea15e1c51993d81209ba6fad72f1ae7
sammy-nguyen <Sambu1227@gmail.com>
2021-10-26 12:04:20 -0600

    added 1

--- server.js
+++ server.js
@@ -3,16 +3,38 @@ const path = require("path");
 const Rollbar = require("rollbar");
 const app = express();
 
+
+
 let rollbar = new Rollbar({
   accessToken: "5b23a0aec9ae427db1a3d6da67dfd333",
   captureUncaught: true,
   captureUnhandledRejections: true,
 });
 
+
+
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
   rollbar.info("html file served successfully");
 });
 
-const port = process.env.PORT || 4545;
+
+
+
+let students = []
+app.post('/api/student', (rep,res) => {
+    const {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log('Student added successfully', {author:'Sam', type: 'manual entry'})
+    res.status(200).send(students)
+
+})
+
+app.use(rollbar.errorHandler())
+
+const port = process.env.PORT || 4545
+
 app.listen(port, () => console.log(`take us to warp ${port}!`));
